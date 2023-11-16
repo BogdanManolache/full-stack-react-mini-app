15c0874335fbad176fc15e437b82720b413412d5
Bogdan2810 <121561815+Bogdan2810@users.noreply.github.com>
2023-11-16 16:54:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Description
+
+This is a small training project built using React for the frontend and using Supabase as a backend.
---
+++ src/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.REACT_APP_DB_URL;
+const supabaseKey = process.env.REACT_APP_DB_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
