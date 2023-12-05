e1d5a9ce2bef564b4c42c50da5dcb712ae25b7b3
Bogdan Manolache <121561815+BogdanManolache@users.noreply.github.com>
2023-12-05 17:30:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -363,19 +363,4 @@ function Fact({ fact, setFacts }) {
 
 export default App;
 
-///////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////
-/*
-function Counter() {
-  const [count, setCount] = useState(0);
 
-  return (
-    <div>
-      <span style={{ fontSize: '40px' }}>{count}</span>
-      <button className="btn btn-large" onClick={() => setCount(c => c + 1)}>
-        +1
-      </button>
-    </div>
-  );
-}
-*/
